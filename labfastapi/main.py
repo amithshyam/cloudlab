0e5dbd08f60f21830e2d2e8b60f9ae99af4f1ba5
amithshyam <amithshyam6151@gmail.com>
2025-03-17 21:10:37 +0530

    Add files via upload

--- labfastapi/main.py
+++ labfastapi/main.py
@@ -1,1 +1,13 @@
-wxs
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse  # Add this line
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent  # Get the directory of main.py
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
